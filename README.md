053eb6c92a0c22d5e4537da220c3b86a71d178bf
ricardoAsilva2003 <ricardo.andre.santos.silva@gmail.com>
2020-05-11 22:20:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-rjit1f-nu8afk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-rjit1f-nu8afk)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Jordan IX',
+    price: 799,
+    description: ''
+  },
+  {
+    name: 'Jordan Retro XII',
+    price: 699,
+    description: ''
+  },
+  {
+    name: 'Jordan I',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
